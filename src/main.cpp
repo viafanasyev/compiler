e738d5d9369239bb07797672ea709b8fd20ca475
viafanasyev <voafanasev@edu.hse.ru>
2021-01-14 18:48:09 +0300

    Added ability to run compiled program

--- src/main.cpp
+++ src/main.cpp
@@ -8,65 +8,54 @@
 #include "frontend/SyntaxError.h"
 #include "MappedFile.h"
 #include "middleend/ast-optimizers.h"
+#include "stack-machine/src/arg-parser.h"
+#include "stack-machine/src/stack-machine.h"
 
-static constexpr size_t maxFileNameLength = 256;
-static const char* assemblyFileExtension = ".asm";
-
-static void stripExtension(char* fileName) {
-    assert(fileName != nullptr);
-
-    char* end = fileName + strlen(fileName);
-
-    while ((end > fileName) && (*end != '.') && (*end != '\\') && (*end != '/')) {
-        --end;
-    }
-
-    if ((end > fileName) && (*end == '.') && (*(end - 1) != '\\') && (*(end - 1) != '/')) {
-        *end = '\0';
-    }
-}
-
-static void replaceExtension(char* destination, const char* originalFileName, const char* newExtension) {
-    assert(originalFileName != nullptr);
-    assert(newExtension != nullptr);
-
-    char tmp[maxFileNameLength];
-    strcpy(tmp, originalFileName);
-    stripExtension(tmp);
-    strcpy(destination, strcat(tmp, newExtension));
-}
+const char* const irFileExtension = ".ir";
 
 void outputAST(const std::shared_ptr<ASTNode>& root, const char* fileName) {
     root->visualize(fileName);
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Invalid arguments number (argc = %d). Expected filename", argc);
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Invalid arguments number (argc = %d). Expected filename or filename and mode", argc);
         return -1;
     }
     const char* codeFileName = argv[1];
     MappedFile file(codeFileName);
+    bool runAfterCompilation = (argc >= 3) && (strcmp(argv[2], "run") == 0);
 
     auto optimizer = std::make_shared<CompositeOptimizer>();
     optimizer->addOptimizer(std::make_shared<UnaryAdditionOptimizer>());
     optimizer->addOptimizer(std::make_shared<ArithmeticNegationOptimizer>());
     optimizer->addOptimizer(std::make_shared<TrivialOperationsOptimizer>());
 
+    int exitCode = 0;
     try {
         std::shared_ptr<ASTNode> ASTRoot = buildASTRecursively(file.getTextPtr());
         ASTRoot = optimizer->optimize(ASTRoot);
         outputAST(ASTRoot, codeFileName);
 
-        char assemblyFileName[maxFileNameLength];
-        replaceExtension(assemblyFileName, codeFileName, assemblyFileExtension);
-        codegen(ASTRoot, assemblyFileName);
+        char irFileName[maxFileNameLength];
+        replaceExtension(irFileName, codeFileName, irFileExtension);
+        codegen(ASTRoot, irFileName);
+
+        if (runAfterCompilation) {
+            char assemblyFileName[maxFileNameLength];
+            replaceExtension(assemblyFileName, codeFileName, assemblyFileExtension);
+
+            exitCode = assemble(irFileName, assemblyFileName);
+            if (exitCode == 0) exitCode = run(assemblyFileName);
+
+            if (exitCode != 0) printErrorMessageForExitCode(exitCode);
+        }
     } catch (const std::logic_error& ex) {
         fprintf(stderr, "Invalid expression: %s", ex.what());
-        return -1;
+        exitCode = -1;
     } catch (const SyntaxError& ex) {
         fprintf(stderr, "Syntax error: %s", ex.what());
-        return -1;
+        exitCode = -1;
     }
-    return 0;
+    return exitCode;
 }
